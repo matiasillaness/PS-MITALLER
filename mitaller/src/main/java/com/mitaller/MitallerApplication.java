8c61c595b6b0c2f0a6476f880b7dba78a67844cd
matiasillaness <illanesmatias04@gmail.com>
2024-03-28 00:05:37 -0300

    configuracion inicial backend

---
+++ mitaller/src/main/java/com/mitaller/MitallerApplication.java
@@ -0,0 +1,13 @@
+package com.mitaller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MitallerApplication.class, args);
+	}
+
+}
---
+++ mitaller/src/test/java/com/mitaller/MitallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
